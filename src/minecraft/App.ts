e0000cbfaa7a6b7d242e64b823c64c6c32fb51bd
Pranav Mereddy <pranav@wireless-10-155-215-208.public.utexas.edu>
2024-04-17 12:30:53 -0500

    app chunk refactor

--- src/minecraft/App.ts
+++ src/minecraft/App.ts
@@ -18,7 +18,7 @@ import { Chunk } from "./Chunk.js";
 export class MinecraftAnimation extends CanvasAnimation {
   private gui: GUI;
   
-  chunk : Chunk;
+  chunk : Chunk[];
   
   /*  Cube Rendering */
   private cubeGeometry: Cube;
@@ -33,8 +33,18 @@ export class MinecraftAnimation extends CanvasAnimation {
   // Player's head position in world coordinate.
   // Player should extend two units down from this location, and 0.4 units radially.
   private playerPosition: Vec3;
-  
-  
+
+  private oldX: number;
+  private oldY: number;
+  private oldTime: number;
+  private timeOfDay: boolean;
+  private ambient: number;
+  private speed: number;
+  private bgSpeed: number;
+  private vel: number;
+  private isInAir: boolean;
+  private tp: boolean;
+
   constructor(canvas: HTMLCanvasElement) {
     super(canvas);
 
@@ -46,15 +56,47 @@ export class MinecraftAnimation extends CanvasAnimation {
     this.gui = new GUI(this.canvas2d, this);
     this.playerPosition = this.gui.getCamera().pos();
     
-    // Generate initial landscape
-    this.chunk = new Chunk(0.0, 0.0, 64);
-    
     this.blankCubeRenderPass = new RenderPass(gl, blankCubeVSText, blankCubeFSText);
     this.cubeGeometry = new Cube();
     this.initBlankCube();
     
     this.lightPosition = new Vec4([-1000, 1000, -1000, 1]);
-    this.backgroundColor = new Vec4([0.0, 0.37254903, 0.37254903, 1.0]);    
+    this.backgroundColor = new Vec4([0.0, 0.749, 1.0, 1.0]);
+
+    this.oldTime = performance.now() / 1000;
+    this.timeOfDay = false;
+    this.ambient = 1.0;
+    this.speed = 100;
+    this.bgSpeed = 50;
+    this.vel = 0;
+    this.isInAir = true;
+    this.tp = false;
+  }
+
+  public turnTP() {
+    this.tp = !this.tp;
+  }
+
+  public swapTime(){
+    this.timeOfDay = !this.timeOfDay;
+  }
+
+  public updateSpeed(delta: number){
+    this.speed = this.speed * delta;
+  }
+
+  private load(x: number, y: number) {
+    if(x == this.oldX && y == this.oldY) {
+      return;
+    }
+    this.chunk = [];
+    for(let i = 0; i <= 2; i++){
+      for(let j = 0; j <= 2; j++){
+        this.chunk.push(new Chunk((64 * (i - 1)) + x, (64 * (j - 1)) + y, 64));
+      }
+    }
+    this.oldX = x;
+    this.oldY = y;
   }
 
   /**
@@ -62,11 +104,14 @@ export class MinecraftAnimation extends CanvasAnimation {
    */
   public reset(): void {    
       this.gui.reset();
-      
+      this.timeOfDay = false;
+      this.tp = false;
+      this.ambient = 1.0;
+      this.speed = 100;
       this.playerPosition = this.gui.getCamera().pos();
-      
+      this.backgroundColor = new Vec4([0.0, 0.749, 1.0, 1.0]);
+      this.load(0, 0);
   }
-  
   
   /**
    * Sets up the blank cube drawing
@@ -113,6 +158,26 @@ export class MinecraftAnimation extends CanvasAnimation {
       new Float32Array(0)
     );
 
+    this.blankCubeRenderPass.addInstancedAttribute("aType",
+      1,
+      this.ctx.FLOAT,
+      false,
+      1 * Float32Array.BYTES_PER_ELEMENT,
+      0,
+      undefined,
+      new Float32Array(0)
+    );
+
+    this.blankCubeRenderPass.addInstancedAttribute("aTime",
+      1,
+      this.ctx.FLOAT,
+      false,
+      1 * Float32Array.BYTES_PER_ELEMENT,
+      0,
+      undefined,
+      new Float32Array(0)
+    );
+
     this.blankCubeRenderPass.addUniform("uLightPos",
       (gl: WebGLRenderingContext, loc: WebGLUniformLocation) => {
         gl.uniform4fv(loc, this.lightPosition.xyzw);
@@ -124,6 +189,10 @@ export class MinecraftAnimation extends CanvasAnimation {
     this.blankCubeRenderPass.addUniform("uView",
       (gl: WebGLRenderingContext, loc: WebGLUniformLocation) => {
         gl.uniformMatrix4fv(loc, false, new Float32Array(this.gui.viewMatrix().all()));
+    });
+    this.blankCubeRenderPass.addUniform("uAmbient",
+      (gl: WebGLRenderingContext, loc: WebGLUniformLocation) => {
+        gl.uniform1f(loc, this.ambient);
     });
     
     this.blankCubeRenderPass.setDrawData(this.ctx.TRIANGLES, this.cubeGeometry.indicesFlat().length, this.ctx.UNSIGNED_INT, 0);
@@ -137,14 +206,96 @@ export class MinecraftAnimation extends CanvasAnimation {
    *
    */
   public draw(): void {
-    //TODO: Logic for a rudimentary walking simulator. Check for collisions and reject attempts to walk into a cube. Handle gravity, jumping, and loading of new chunks when necessary.
-    this.playerPosition.add(this.gui.walkDir());
-    
-    this.gui.getCamera().setPos(this.playerPosition);
+    let x: number = this.playerPosition.x;
+    let z: number = this.playerPosition.z;
+
+    let pHeight = this.playerPosition.y - 2;
+    let time = performance.now() / 1000 - this.oldTime;
+    this.oldTime = performance.now() / 1000;
+
+    if(this.chunk){
+      let curHeight = this.chunk[4].cubePositions()[(Math.floor(z - this.chunk[4].getTopLeftZ()) * 64 + Math.floor(x - this.chunk[4].getTopLeftX())) * 4 + 1];
+      if (this.isInAir) {
+        pHeight += this.vel * time;
+        this.vel -= 9.8 * time;
+
+        if (pHeight < curHeight) {
+          pHeight = curHeight;
+          this.vel = 0;
+          this.isInAir = false;
+        }
+        this.playerPosition.add(this.gui.walkDir());
+        this.playerPosition.y = pHeight + 2;
+        this.gui.getCamera().setPos(this.playerPosition);
+      }
+      else {
+        this.vel = 0;
+      }
+      let movedPos = Vec3.sum(this.playerPosition, this.gui.walkDir());
+      let secIndex = 4;
+      if(Vec3.sum(this.playerPosition, this.gui.walkDir().scale(1.4)).x < this.chunk[4].getTopLeftX()) {
+        if(Vec3.sum(this.playerPosition, this.gui.walkDir().scale(1.4)).z >= this.chunk[4].getTopLeftZ() + 64){
+          secIndex = 2;
+        }
+        else if(Vec3.sum(this.playerPosition, this.gui.walkDir().scale(1.4)).z < this.chunk[4].getTopLeftZ()){
+          secIndex = 0;
+        }
+        else{
+          secIndex = 1;
+        }
+      }
+      else if (Vec3.sum(this.playerPosition, this.gui.walkDir().scale(1.4)).x >= this.chunk[4].getTopLeftX() + 64){
+        if(Vec3.sum(this.playerPosition, this.gui.walkDir().scale(1.4)).z >= this.chunk[4].getTopLeftZ() + 64){
+          secIndex = 8;
+        }
+        else if(Vec3.sum(this.playerPosition, this.gui.walkDir().scale(1.4)).z < this.chunk[4].getTopLeftZ()){
+          secIndex = 6;
+        }
+        else{
+          secIndex = 7;
+        }
+      }
+      else{
+        if(Vec3.sum(this.playerPosition, this.gui.walkDir().scale(1.4)).z < this.chunk[4].getTopLeftZ()){
+          secIndex= 3;
+        }
+        else if(Vec3.sum(this.playerPosition, this.gui.walkDir().scale(1.4)).z >= this.chunk[4].getTopLeftZ()+64){
+          secIndex = 5;
+        }
+      } 
+      let iVal = (Math.floor(Vec3.sum(this.playerPosition, this.gui.walkDir().scale(1.4)).z - this.chunk[secIndex].getTopLeftZ()) * 64 + Math.floor(Vec3.sum(this.playerPosition, this.gui.walkDir().scale(1.4)).x - this.chunk[secIndex].getTopLeftX())) * 4 + 1;
+      let neighborHeight = this.chunk[secIndex].cubePositions()[iVal];
+      if (neighborHeight <= pHeight) {
+        this.playerPosition = movedPos;
+        this.gui.getCamera().setPos(this.playerPosition);
+        if(neighborHeight < pHeight){
+          this.isInAir = true;   
+        }
+      }
+    }
+    else {
+      this.playerPosition.add(this.gui.walkDir());
+      this.gui.getCamera().setPos(this.playerPosition);
+    }
+
+    if (this.timeOfDay) {
+      if((this.lightPosition.y >= 1000) || (this.lightPosition.y <= 0)){
+        this.bgSpeed =- this.bgSpeed;
+        this.speed =- this.speed;
+      }
+      this.lightPosition.x -= this.speed * time;
+      this.lightPosition.y += this.speed * time;
+      this.lightPosition.z -= this.speed * time;
+      this.ambient = (this.lightPosition.y + 10) / 1050;
+    }
     
     // Drawing
     const gl: WebGLRenderingContext = this.ctx;
-    const bg: Vec4 = this.backgroundColor;
+    let bg: Vec4 = this.backgroundColor;
+    if(this.timeOfDay){
+      this.backgroundColor.y = (1200 - this.lightPosition.y) / 1200 *  0.749;
+      this.backgroundColor.z = (1200 - this.lightPosition.y) / 1200 * 0.749;
+    }
     gl.clearColor(bg.r, bg.g, bg.b, bg.a);
     gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
     gl.enable(gl.CULL_FACE);
@@ -152,18 +303,121 @@ export class MinecraftAnimation extends CanvasAnimation {
     gl.frontFace(gl.CCW);
     gl.cullFace(gl.BACK);
 
-    gl.bindFramebuffer(gl.FRAMEBUFFER, null); // null is the default frame buffer
-    this.drawScene(0, 0, 1280, 960);        
+    gl.bindFramebuffer(gl.FRAMEBUFFER, null); 
+    this.drawScene(x, z, 1280, 960);        
   }
 
   private drawScene(x: number, y: number, width: number, height: number): void {
     const gl: WebGLRenderingContext = this.ctx;
-    gl.viewport(x, y, width, height);
+    gl.viewport(0, 0, width, height);
+    x = Math.floor((x + 32) / 64) * 64;
+    y = Math.floor((y + 32) / 64) * 64;
+    this.load(x, y);
 
     //TODO: Render multiple chunks around the player, using Perlin noise shaders
-    this.blankCubeRenderPass.updateAttributeBuffer("aOffset", this.chunk.cubePositions());
-    this.blankCubeRenderPass.drawInstanced(this.chunk.numCubes());    
+    let bTypes: number[] = [];
+    let bTimes: number[] = [];
+    let bList: number[] = [];
+    let time = 50.0;
+    if (this.tp) {
+      time = Math.floor(performance.now() / 1000);
+    }
+    let c: number = 0;
+    for(let i = 0; i < this.chunk.length; i++){
+      c += this.chunk[i].numCubes();
+      for(let j = 0; j < this.chunk[i].cubePositions().length; j++){
+        bList.push(this.chunk[i].cubePositions()[j]);
+        bTimes.push(time);
+      }
+      for(let j = 0; j < this.chunk[i].type().length; j++){
+        bTypes.push(this.chunk[i].type()[j]);
+      }
+    }
+    c += this.fillGaps(bList, bTypes);
 
+    const blocks = new Float32Array(bList);
+    const types = new Float32Array(bTypes);
+    const times = new Float32Array(bTimes);
+
+    this.blankCubeRenderPass.updateAttributeBuffer("aOffset", blocks);
+    this.blankCubeRenderPass.updateAttributeBuffer("aType", types);
+    this.blankCubeRenderPass.updateAttributeBuffer("aTime", times);
+
+    this.blankCubeRenderPass.drawInstanced(c);    
+  }
+
+  private fillGaps(blocks: number[], types: number[]): number {
+    let bList = blocks.slice();
+    let c = 0;
+
+    for(let i = 1; i < bList.length; i += 4){
+      if(((i - 1) / 192) + 1 % 192 != 0) {
+        if(Math.abs(bList[i] - bList[i+4]) > 1) {
+          let higher;
+          if (bList[i] > bList[i + 4]) {
+              higher = i;
+          } else {
+              higher = i + 4;
+          }
+          let sub = Math.abs(bList[i] - bList[i+4]) - 1;
+          while(sub != 0) {
+            let height = bList[higher] - sub;
+            blocks.push(bList[higher - 1]);
+            blocks.push(height);
+            blocks.push(bList[higher + 1]);
+            blocks.push(bList[higher + 2]);
+            --sub;
+
+            if(height <= 10) {
+              types.push(0);
+            }
+            else if(height <= 20) {
+              types.push(1)
+            }
+            else if(height <= 45) {
+              types.push(2);
+            }
+            else {
+              types.push(3);
+            }
+            c++;
+          }
+        }
+      }
+      if((i + 192 * 4) < bList.length) {
+        if(Math.abs(bList[i] - bList[(i + 192 * 4)]) > 1) {
+          let higher;
+          if (bList[i] > bList[(i + 192 * 4)]) {
+              higher = i;
+          } else {
+              higher = i + 192 * 4;
+          }
+          let sub = Math.abs(bList[i] - bList[(i + 192 * 4)]) - 1;
+          while(sub != 0) {
+            let height = bList[higher] - sub;
+            blocks.push(bList[higher - 1]);
+            blocks.push(height);
+            blocks.push(bList[higher + 1]);
+            blocks.push(bList[higher + 2]);
+            --sub;
+            if(height <= 10) {
+              types.push(0);
+            }
+            else if(height <= 20) {
+              types.push(1)
+            }
+            else if(height <= 45) {
+              types.push(2);
+            }
+            else {
+              types.push(3);
+            }
+            c++;
+          }
+        }
+      }
+    }
+    return c;
   }
 
   public getGUI(): GUI {
@@ -172,7 +426,10 @@ export class MinecraftAnimation extends CanvasAnimation {
   
   
   public jump() {
-      //TODO: If the player is not already in the lair, launch them upwards at 10 units/sec.
+      if(!this.isInAir) {
+        this.isInAir = true;
+        this.vel = 10;
+      }
   }
 }
 
--- src/minecraft/Chunk.ts
+++ src/minecraft/Chunk.ts
@@ -7,39 +7,175 @@ export class Chunk {
     private x : number; // Center of the chunk
     private y : number;
     private size: number; // Number of cubes along each side of the chunk
-    
+
+    private heightMax: number;
+    private typeOfBlock: number[];
+    private seed: string;
+    private octaves: number[][][];
+
     constructor(centerX : number, centerY : number, size: number) {
         this.x = centerX;
         this.y = centerY;
         this.size = size;
-        this.cubes = size*size;        
-        this.generateCubes();
-    }
-    
-    
-    private generateCubes() {
-        const topleftx = this.x - this.size / 2;
-        const toplefty = this.y - this.size / 2;
-        
-      //TODO: The real landscape-generation logic. The example code below shows you how to use the pseudorandom number generator to create a few cubes.
-      this.cubes = this.size * this.size;
-      this.cubePositionsF32 = new Float32Array(4 * this.cubes);
+        this.cubes = size * size; 
 
-      const seed = "42";
-      let rng = new Rand(seed);
-      for(let i=0; i<this.size; i++)
-      {
-          for(let j=0; j<this.size; j++)
-          {
-            const height = Math.floor(10.0 * rng.next());
-            const idx = this.size * i + j;
-            this.cubePositionsF32[4*idx + 0] = topleftx + j;
-            this.cubePositionsF32[4*idx + 1] = height;
-            this.cubePositionsF32[4*idx + 2] = toplefty + i;
-            this.cubePositionsF32[4*idx + 3] = 0;
-          }
-      }
-    
+        this.heightMax = 100.0;
+        this.typeOfBlock = [];
+        this.seed = this.generateHash(this.x, this.y).toString();
+        this.octaves = [];
+        this.octOne();
+        this.octPop();
+        this.generateChunks();
+    }
+
+    private generateChunks() {
+        this.cubes = this.size * this.size;
+        this.cubePositionsF32 = new Float32Array(4 * this.cubes);
+        for(let i = 0; i < this.size; i++){
+            for(let j = 0; j < this.size; j++){
+                let h: number = 0;
+                for(let k = 0; k < 3; k++){
+                    h += 0.2 * this.bInter(i, j, 32 * Math.pow(1/2, k), this.octaves[k]);
+                }
+                h = Math.floor(h);
+                if(h <= 10){
+                    this.typeOfBlock.push(0);
+                    h = 10;
+                }
+                else if(h <= 20) {
+                    this.typeOfBlock.push(1)
+                }
+                else if(h <= 45) {
+                    this.typeOfBlock.push(2);
+                }
+                else {
+                    this.typeOfBlock.push(3);
+
+                }
+                this.cubePositionsF32[4 * (this.size * i + j)] = this.getTopLeftX() + j;
+                this.cubePositionsF32[4 * (this.size * i + j) + 1] = h;
+                this.cubePositionsF32[4 * (this.size * i + j) + 2] = this.getTopLeftZ() + i;
+                this.cubePositionsF32[4 * (this.size * i + j) + 3] = 0;
+            }
+        }
+    }
+
+    public getTopLeftX(): number{
+        return this.x - this.size / 2;
+    }
+
+    public getTopLeftZ(): number{
+        return this.y - this.size / 2;
+    }
+
+    private octPop() {
+        const oct22: number[][] = this.octaves[0];
+        let oct44: number[][] = [];
+        let oct88: number[][] = [];
+
+        for (let i = 0; i < 4; i++) {
+            oct44.push([]);
+            for(let j = 0; j < 4; j++){
+                if (i % 2 == 0 && j % 2 == 0){
+                    oct44[i].push(oct22[i/2][j/2]);
+                    continue;
+                }
+                oct44[i].push(Math.floor(this.bInter(i, j, 2, oct22)));
+            }
+        }
+
+        oct44.push([]);
+
+        for(let i = 0; i < 5; i++) {
+            if(i % 2 == 0) {
+                oct44[4].push(oct22[2][i/2]);
+                continue;
+            }
+            oct44[4].push(Math.floor((oct22[2][Math.floor(i/2)] + oct22[2][Math.ceil(i/2)]) / 2.0));
+        }
+
+        for(let i = 0; i < 4; i++) {
+            if(i % 2 == 0) {
+                oct44[i].push(oct22[i/2][2]);
+                continue;
+            }
+            oct44[i].push(Math.floor((oct22[Math.floor(i/2)][2] + oct22[Math.ceil(i/2)][2]) / 2.0));
+        }
+        this.octaves.push(oct44);
+
+        for(let i = 0; i < 8; i++){
+            oct88.push([]);
+            for(let j = 0; j < 8; j++){
+                if(i % 2 == 0 && j % 2 == 0){
+                    oct88[i].push(oct44[i/2][j/2]);
+                    continue;
+                }
+                oct88[i].push(this.bInter(i, j, 2, oct44));
+            }
+        }
+        oct88.push([]);
+
+        for(let i = 0; i < 9; i++){
+            if(i % 2 == 0){
+                oct88[8].push(oct44[4][i/2]);
+                continue;
+            }
+            oct88[8].push((oct44[4][Math.floor(i/2)] + oct44[4][Math.ceil(i/2)]) / 2.0);
+        }
+
+        for(let i = 0; i < 8; i++){
+            if(i % 2 == 0){
+                oct88[i].push(oct44[i/2][4]);
+                continue;
+            }
+            oct88[i].push((oct44[Math.floor(i/2)][4] + oct44[Math.ceil(i/2)][4]) / 2.0);
+        }
+
+        this.octaves.push(oct88);
+    }
+
+    private bInter(x: number, y: number, len: number, octave: number[][]): number {
+        return (1 - ((x % len) / len)) * (1 - ((y % len) / len)) * octave[Math.floor(x / len)][Math.floor(y / len)] + 
+        ((x % len) / len) * (1 - ((y % len) / len)) * octave[Math.floor(x / len)+1][Math.floor(y / len)] + 
+        (1 - ((x % len) / len)) * ((y % len) / len) * octave[Math.floor(x / len)][Math.floor(y / len)+1] + 
+        ((x % len) / len) * ((y % len) / len) * octave[Math.floor(x / len) + 1][Math.floor(y / len) + 1];
+    }
+
+    private octOne() {
+        let oct22: number[][] = [[],[],[]];
+        
+        let rand = new Rand(this.seed);
+        for(let i = 0; i < 2; i++){
+            for(let j = 0; j < 2; j++){
+                oct22[i].push(Math.floor(this.heightMax * rand.next()));
+            }
+        }
+        let rand2 = new Rand(this.generateHash(this.x + 64, this.y).toString());
+        oct22[0].push(Math.floor(this.heightMax * rand2.next()));
+        rand2.next();
+        oct22[1].push(Math.floor(this.heightMax * rand2.next()));
+
+        let rand3 = new Rand(this.generateHash(this.x, this.y + 64).toString());
+        oct22[2].push(Math.floor(this.heightMax * rand3.next()));
+        oct22[2].push(Math.floor(this.heightMax * rand3.next()));
+
+        let rand4 = new Rand(this.generateHash(this.x + 64, this.y + 64).toString());
+        oct22[2].push(Math.floor(this.heightMax * rand4.next()));
+        this.octaves.push(oct22);
+    }
+
+    private generateHash(x: number, y: number): number {
+        let h: number = (x << 4) + y;
+        h += (h << 10);
+        h ^= (h >> 6);
+        h += (h << 3);
+        h ^= (h >> 11);
+        h += (h << 15);
+        return h;  
+    }
+
+    public type(): number[] {
+        return this.typeOfBlock;
     }
     
     public cubePositions(): Float32Array {
